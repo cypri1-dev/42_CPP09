75441c3d4a375f426bdfe06ae0dfaa9b51db5f20
Cyprien Ferreira <cyferrei@paul-f4Br7s2.clusters.42paris.fr>
2025-01-03 13:59:08 +0100

    [ADD]: add checker function to verify numbers of args and extension file - also add excecption in case of error

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -6,7 +6,23 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 12:59:19 by cyferrei          #+#    #+#             */
-/*   Updated: 2025/01/03 12:59:20 by cyferrei         ###   ########.fr       */
+/*   Updated: 2025/01/03 13:55:57 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "BitcoinExchange.hpp"
+#include <iostream>
+
+bool is_txt_file(const std::string &file) {
+	size_t pos = file.rfind(".txt");
+	return (pos != std::string::npos) && (file.substr(0, pos).find(".txt") == std::string::npos);
+}
+
+void	checker(int argc, char **argv) {
+	if (argc != 2)
+		throw NotEnoughtArgs();
+	
+	std::string tmp(argv[1]);
+	if (!is_txt_file(tmp))
+		throw WrongFormatInput();
+}
--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -6,7 +6,29 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 12:59:22 by cyferrei          #+#    #+#             */
-/*   Updated: 2025/01/03 12:59:23 by cyferrei         ###   ########.fr       */
+/*   Updated: 2025/01/03 13:40:58 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BITCOIN_EXCHANGE_HPP
+#define BITCOIN_EXCHANGE_HPP
+
+#include <exception>
+
+void	checker(int argc, char **argv);
+
+class NotEnoughtArgs : public std::exception {
+	public:
+		const char* what()const throw() {
+			return "\033[31mError: This program needs 2 arguments!\033[0m";
+		}
+};
+
+class WrongFormatInput : public std::exception {
+	public:
+		const char* what()const throw() {
+			return "\033[31mError: Wrong extension in input\033[0m";
+		}
+};
+
+#endif
