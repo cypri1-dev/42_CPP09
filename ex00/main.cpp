2b4a0275b89f093fb19fb179754cdc2724412dff
Cyprien Ferreira <cyferrei@paul-f4Br7s2.clusters.42paris.fr>
2025-01-03 13:59:48 +0100

    [ADD]: add little main for checks

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,13 +6,20 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 12:59:24 by cyferrei          #+#    #+#             */
-/*   Updated: 2025/01/03 13:01:47 by cyferrei         ###   ########.fr       */
+/*   Updated: 2025/01/03 13:27:31 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(int argc, char **argv) {
-	(void)argc;
-	(void)argv;
+#include "BitcoinExchange.hpp"
+#include <iostream>
 
+int main(int argc, char **argv) {
+	
+	try {
+		checker(argc, argv);
+	}
+	catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+	}
 	return (0);
 }
