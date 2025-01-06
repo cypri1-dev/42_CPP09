825904e4bc85a33894b701203cbb4e9afc309d59
Cyprien Ferreira <cyferrei@paul-f4Br6s8.clusters.42paris.fr>
2025-01-06 13:23:27 +0100

    [ADD]: add new exception for CSV file - also add implementation of expot_csv

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -6,14 +6,16 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 12:59:19 by cyferrei          #+#    #+#             */
-/*   Updated: 2025/01/03 13:55:57 by cyferrei         ###   ########.fr       */
+/*   Updated: 2025/01/06 11:57:45 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
-#include <iostream>
 
-bool is_txt_file(const std::string &file) {
+#include <fstream>
+#include <sstream>
+
+bool	is_txt_file(const std::string &file) {
 	size_t pos = file.rfind(".txt");
 	return (pos != std::string::npos) && (file.substr(0, pos).find(".txt") == std::string::npos);
 }
@@ -25,4 +27,36 @@ void	checker(int argc, char **argv) {
 	std::string tmp(argv[1]);
 	if (!is_txt_file(tmp))
 		throw WrongFormatInput();
+}
+
+void	export_csv(t_data &data) {
+
+	std::string csv_file = CSV_FILE;
+	std::ifstream file(csv_file.c_str());
+	if (!file.is_open())
+		throw CsvFileError();
+
+	std::string line;
+	bool first_line = true;
+	while(std::getline(file, line)) {
+		if (first_line) {
+			first_line = false;
+			continue;
+		}
+		size_t comma_pos = line.find(",");
+		if (comma_pos == std::string::npos) {
+			dprintf(2, "Error!\n");
+			throw CsvFileError();
+		}
+		std::string date = line.substr(0, comma_pos);
+		std::string value_str = line.substr(comma_pos + 1);
+
+		std::istringstream iss(value_str);
+		double value;
+		if (!(iss >> value))
+			throw CsvFileError();
+		
+		data.mapCSV[date] = value;
+	}
+	file.close();
 }
--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 12:59:22 by cyferrei          #+#    #+#             */
-/*   Updated: 2025/01/03 13:40:58 by cyferrei         ###   ########.fr       */
+/*   Updated: 2025/01/06 11:52:14 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,17 @@
 #define BITCOIN_EXCHANGE_HPP
 
 #include <exception>
+#include <map>
+#include <string>
+
+#define CSV_FILE "data.csv"
+
+typedef struct s_data {
+	std::map<std::string, double> mapCSV;
+} t_data;
 
 void	checker(int argc, char **argv);
+void	export_csv(t_data &data);
 
 class NotEnoughtArgs : public std::exception {
 	public:
@@ -28,6 +37,13 @@ class WrongFormatInput : public std::exception {
 	public:
 		const char* what()const throw() {
 			return "\033[31mError: Wrong extension in input\033[0m";
+		}
+};
+
+class CsvFileError : public std::exception {
+	public:
+		const char *what()const throw() {
+			return "\033[31mError: Can't load CSV data!\033[0m";
 		}
 };
 
