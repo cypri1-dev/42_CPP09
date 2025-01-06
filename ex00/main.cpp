93e1d46d1ca83fd2d0006750fa5bbaf3adf876ac
cyferrei <ferreiracyprien@gmail.com>
2025-01-06 18:45:04 +0100

    [ADD]: add convertBtc function

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 12:59:24 by cyferrei          #+#    #+#             */
-/*   Updated: 2025/01/06 11:51:30 by cyferrei         ###   ########.fr       */
+/*   Updated: 2025/01/06 17:10:32 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,13 @@ int main(int argc, char **argv) {
 	
 	t_data data;
 	try {
-		// ? little parsing
-		checker(argc, argv);
-		// TODO: check permissions
-		// TODO: export .csv in a map
+		checker_args(argc, argv);
 		export_csv(data);
-		for (std::map<std::string, double>::iterator it = data.mapCSV.begin(); it != data.mapCSV.end(); ++it) {
-					std::cout << "Date: " << it->first << ", Taux de change: " << it->second << std::endl;
-				}
+		// for (std::map<std::string, double>::iterator it = data.mapCSV.begin(); it != data.mapCSV.end(); ++it) {
+		// 	std::cout << "Date: " << it->first << ", Taux de change: " << it->second << std::endl;
+		// }
+		convert_btc(data);
 	}
-	// ! if fail throw exception
 	catch (std::exception &e) {
 		std::cout << e.what() << std::endl;
 	}
