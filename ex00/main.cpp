a5f767b0d4ab7eaacea5e001fcc1fdad7b2130b2
Cyprien Ferreira <cyferrei@paul-f4Br6s8.clusters.42paris.fr>
2025-01-06 13:22:42 +0100

    [ADD]: add commentary - also add export_csv function - seems ok - need to test it

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cyferrei <cyferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 12:59:24 by cyferrei          #+#    #+#             */
-/*   Updated: 2025/01/03 13:27:31 by cyferrei         ###   ########.fr       */
+/*   Updated: 2025/01/06 11:51:30 by cyferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,21 @@
 
 int main(int argc, char **argv) {
 	
+	t_data data;
 	try {
+		// ? little parsing
 		checker(argc, argv);
+		// TODO: check permissions
+		// TODO: export .csv in a map
+		export_csv(data);
+		for (std::map<std::string, double>::iterator it = data.mapCSV.begin(); it != data.mapCSV.end(); ++it) {
+					std::cout << "Date: " << it->first << ", Taux de change: " << it->second << std::endl;
+				}
 	}
+	// ! if fail throw exception
 	catch (std::exception &e) {
 		std::cout << e.what() << std::endl;
 	}
+	
 	return (0);
 }
